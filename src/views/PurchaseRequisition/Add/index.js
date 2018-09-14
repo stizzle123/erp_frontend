27497f80b25b8605cc432e79fdf1e3fddc8b469c
kolawole abobade <kolawole.abobade@gmail.com>
2018-09-14 08:43:32 +0100

    Cleaned up teh Purchase requisitionsection, line items now properly adding, Purchasing Workflow WIP

--- src/views/PurchaseRequisition/Add/index.js
+++ src/views/PurchaseRequisition/Add/index.js
@@ -5,6 +5,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
 import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
@@ -16,21 +19,33 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
 import CustomSelect from "components/CustomInput/CustomSelect.jsx";
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
 import Remove from "@material-ui/icons/Remove";
 import Add from "@material-ui/icons/Add";
 import Close from "@material-ui/icons/Close";
 import classes from "classnames";
-import Table from "components/Table/Table.jsx";
+import {connect} from 'react-redux';
+//import Table from "components/Table/Table.jsx";
 
 import regularFormsStyle from "assets/jss/material-dashboard-pro-react/views/regularFormsStyle.jsx";
+import tableStyle from "assets/jss/material-dashboard-pro-react/components/tableStyle.jsx";
+
 
 const styles = theme => ({
-
+  ...tableStyle,
+  ...regularFormsStyle,
+  td:{
+    padding: '0px',
+  },
 });
 
 
 const categories = [
-  {value: '0', label:'Select Category',},
+  {value: '0', label:'Select',},
   {value: '1',label: 'Category 1',},
   {value: '2',label: 'Category 2',},
   {value: '3',label: 'Category 3',}
@@ -40,12 +55,24 @@ class PurchaseRequisition extends React.Component {
   state = {
     simpleSelect: "",
     type: '',
-    rowArray:[]
+    rowArray:[],
+    data:{
+      requisitiontype:'',
+      requestedby: this.props.user.fullname,
+      employeeid: "",
+      departmentname: "",
+      chargeto: "",
+      dateneeded: ""
+    },
+    lineItems:[]
   };
 
-	handleChange = name => event => {
-		this.setState({ [name]: event.target.checked });	
-
+	handleChange = event => {
+    let data = this.state.data;
+    data[[event.target.id]] = event.target.value; 
+    this.setState({ 
+      data : data,
+    });
 	};
 	
 	increaseRow = event=>{
@@ -61,60 +88,81 @@ class PurchaseRequisition extends React.Component {
   }
 
   handleLineItemChange= event =>{
-    
+    {{debugger}}
+    let lineItems = this.state.lineItems;
+    this.state.rowArray.map((prop, key)=> {
+
+    });
   }
 
   handleSimple = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
   render() {
-    const { classes } = this.props;
+    const { classes, tableHeaderColor } = this.props;
+    {{debugger}}
+    const today = new Date();
   	const tableData = this.state.rowArray.map((prop, key)=> {
       return (
-        [                   
-                    (key+1), 
-                    <CustomSelect labelText="Category" id={key+1} name="category" required
+        <TableRow key={key}> 
+          <TableCell component="th" scope="row">               
+            {key+1}
+          </TableCell>
+          <TableCell className={classes.td}>
+              <CustomSelect labelText="Select" id={key+1+"category"} name="category" required
                      onChange={(e)=>this.handleLineItemChange(e)}
                     formControlProps={{
                       fullWidth: true
                     }} 
-                    inputProps={{margin:"normal"}}
+                    inputProps={{margin:"normal", }}
                     >
                         {categories.map(option => (
                           <MenuItem key={option.value} value={option.value}>
                             {option.label}
                           </MenuItem>
                         ))}
-                      </CustomSelect>,
-                      <CustomInput name="itemDescription" id={key+1} onChange={(e)=>this.handleLineItemChange(e)} required formControlProps={{                    
+              </CustomSelect>
+          </TableCell>
+          <TableCell className={classes.td}>
+                <CustomInput name="itemDescription" id={key+1+"itemdescription"} onChange={(e)=>this.handleLineItemChange(e)} required formControlProps={{  
+                                   
                       }}
-                    />,
-                      <CustomInput name="quantity" id={key+1} onChange={(e)=>this.handleLineItemChange(e)} type="number" required formControlProps={{  
-                        style: {width:"20px"}                  
+                    />
+          </TableCell>
+          <TableCell className={classes.td}>
+                <CustomInput name="quantity" id={key+1+"quantity"} onChange={(e)=>this.handleLineItemChange(e)} type="number" required formControlProps={{  
+                                          
                       }}
-                    />,
-                    <Button
-              color="danger"
-              simple
-              className={classes.actionButton}
-              key={key}
-              onClick={() => {
-              var data = this.state.rowArray;
-              data.find((o, i) => {
-              if (i === key) {
-                // here you should add some custom code so you can delete the data
-                // from this component and from your server as well
-                data.splice(i, 1);
-                return true;
-              }
-                      return false;
-                    });
-                    this.setState({ rowArray: data });
-                  }}
-        >
-          <Close className={classes.icon} />
-        </ Button>
-                    ] 
+                    />
+          </TableCell>
+          <TableCell className={classes.td}>
+                <CustomInput name="unit" id={key+1+"unit"} onChange={(e)=>this.handleLineItemChange(e)} type="number" required formControlProps={{  
+                                    
+                    }}
+                  />
+                  <Button
+                        color="danger"
+                        simple
+                        className={classes.actionButton}
+                        key={key}
+                        onClick={() => {
+                        var data = this.state.rowArray;
+                        data.find((o, i) => {
+                        if (i === key) {
+                          // here you should add some custom code so you can delete the data
+                          // from this component and from your server as well
+                          data.splice(i, 1);
+                          return true;
+                        }
+                                return false;
+                              });
+                              this.setState({ rowArray: data });
+                            }}
+                  >
+                    <Close className={classes.icon} />
+                  </ Button>
+          </TableCell>      
+        </TableRow>
         )}
     );
     let showVendorsName = false; 
@@ -138,7 +186,7 @@ class PurchaseRequisition extends React.Component {
             </CardHeader>
               <CardBody>
               <Grid container>
-                  <GridItem xs={12} sm={6} md={5} lg={5}>
+                  <GridItem xs={12} sm={12} md={6} lg={6}>
                         <FormControl
                           fullWidth
                           className={classes.selectFormControl}
@@ -191,21 +239,54 @@ class PurchaseRequisition extends React.Component {
                             </MenuItem>                                                      
                           </Select>
                         </FormControl>
-                  </GridItem>	                         
-                <GridItem xs={12} sm={4} md={4}>
-                    <CustomInput labelText="Department" id="departmentname" required
-                      formControlProps={{
+                  </GridItem>	
+                  <GridItem xs={12} sm={12} md={6} lg={6}>
+                      Requisition No: 
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                  <CustomInput labelText="Required By" id="requestedby"
+                  formControlProps={{
                       fullWidth: true
-                      }}
-                    />
-                </GridItem>
-              <GridItem xs={12} sm={4} md={4}>
-                  <CustomInput labelText="Charge To" id="chargeto" required formControlProps={{
-                    fullWidth: true
-                    }}
-                  />
-              </GridItem>   
-              <GridItem xs={12} sm={4} md={4}>
+                        }}
+                    inputProps={{ 
+                      disabled: true,
+                      value:"Required : "                  
+                        }}
+                      />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}> 
+                      <CustomInput labelText="Employee ID" id="employeeid"
+                        formControlProps={{
+                          fullWidth: true
+                        }} inputProps={{
+                          disabled: true, value: "Employee ID : "                 
+                        }}
+                      />
+                  </GridItem>   
+                  <GridItem xs={12} sm={12} md={4}>
+                      <CustomInput required formControlProps={{
+                        fullWidth: true
+                        }}
+                        inputProps={{
+                          disabled: true,
+                          value:{today}                 
+                            }}
+                      />
+                  </GridItem>                   
+                  <GridItem xs={12} sm={4} md={4}>
+                      <CustomInput labelText="Department" id="departmentname" required
+                        formControlProps={{
+                        fullWidth: true
+                        }}
+                      />
+                  </GridItem>
+                  <GridItem xs={12} sm={4} md={4}>
+                      <CustomInput labelText="Charge To" id="chargeto" required formControlProps={{
+                        fullWidth: true
+                        }}
+                      />
+                  </GridItem>   
+                  <GridItem xs={12} sm={4} md={4}>
                       <CustomInput labelText="Date Needed" id="dateneeded" required formControlProps={{
                       fullWidth: true
                         }}
@@ -213,7 +294,7 @@ class PurchaseRequisition extends React.Component {
                         }}
                       />
                   </GridItem>              
-                  <GridItem xs={12} sm={8} md={8}>
+                  	{/* <GridItem xs={12} sm={8} md={8}>
                       <CustomInput labelText="Purpose Of Use" id="purpose" required formControlProps={{
                       fullWidth: true
                         }}
@@ -221,26 +302,41 @@ class PurchaseRequisition extends React.Component {
                     inputProps={{                      
                         }}
                       />
-                  </GridItem>            
-                  	{/* 
+                  </GridItem>  */}          
+            
                   <GridItem xs={12} sm={4} md={4}>
                       <CustomInput labelText="Ship Via" id="ship" required formControlProps={{
                         fullWidth: true
                         }}
                       />
-                  </GridItem> */}
+                  </GridItem> 
+                  <GridItem xs={12} sm={4} md={4}>
+                      <CustomInput labelText="Status" id="status" required formControlProps={{
+                        fullWidth: true
+                        }} inputProps={{  
+                          value:"Pending Submission",
+                          disabled:true                  
+                        }}
+                      />
+                  </GridItem> 
               </Grid>
                   <br />  
-                  <Table
-                    tableHead={[
-                      "#",
-                      "Category",
-                      "Item Description",
-                      "Quantity",
-                      "Actions"
-                    ]}
-                    tableData={tableData}
-                  />  
+                  <div className={classes.tableResponsive}>
+                  <Table className={classes.table} style={{width:"100%"}}> 
+                    <TableHead  className={classes[tableHeaderColor + "TableHeader"]}>
+                      <TableRow>
+                        <TableCell className={classes.tableCell + " " + classes.tableHeadCell+" "+classes.td} >Item No</TableCell>
+                        <TableCell className={classes.tableCell + " " + classes.tableHeadCell+" "+classes.td}>Category</TableCell>
+                        <TableCell className={classes.tableCell + " " + classes.tableHeadCell+" "+classes.td}>Item Description</TableCell>
+                        <TableCell className={classes.tableCell + " " + classes.tableHeadCell+" "+classes.td}>Quantity</TableCell>
+                        <TableCell className={classes.tableCell + " " + classes.tableHeadCell+" "+classes.td}>Unit</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {tableData}
+                    </TableBody>
+                  </Table> 
+                </div>
 			          <Button
                   justIcon
                   round
@@ -270,9 +366,22 @@ class PurchaseRequisition extends React.Component {
   }
 }
 
+PurchaseRequisition.defaultProps = {
+  tableHeaderColor: "gray"
+};
+
 PurchaseRequisition.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(regularFormsStyle)(PurchaseRequisition);
+
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user,
+    loading: state.loader.loading,
+    loader : state.loader
+  };
+}
+
+export default connect(mapStateToProps, null)(withStyles(styles)(PurchaseRequisition));
 
