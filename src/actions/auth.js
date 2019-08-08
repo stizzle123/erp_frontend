c776dd2b0d722c255dad1b74d9f85308a4519fbb
Idowu <iarifayan@russelsmith.local>
2019-08-08 11:38:39 +0100

    Made changes to the auth.js file in actions directory

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,23 +1,24 @@
 import MiddleWare from "../middleware/api";
 
 let AclAuth = {
-    authenticate(username, password, cb) {
-      let middleware = new MiddleWare();
-      let data = {};
-      data.email = username;
-      data.password = password;
-      middleware.makeConnection('/users/login','POST', data).then((response) => {
-        if(response.ok && response.status == 200){
-         return response.json();
-        }else{
+  authenticate(username, password, cb) {
+    let middleware = new MiddleWare();
+    let data = {};
+    data.email = username;
+    data.password = password;
+    middleware
+      .makeConnection("/users/login", "POST", data)
+      .then(response => {
+        if (response.ok && response.status == 200) {
+          return response.json();
+        } else {
           cb(response);
         }
-      }).then(
-        (user)=>{   
-            cb('', user.user, user.token);
-        }
-      )
-      /* let user = {};
+      })
+      .then(user => {
+        cb("", user.user, user.token);
+      });
+    /* let user = {};
       if(username === "admin" && password === "password"){
         user = {id: 1, username:"admin", role: "admin", email: "admin@russelsmithgroup.com"};
       }else if(username === "vendor" && password === "password"){
@@ -27,10 +28,10 @@ let AclAuth = {
       }else if(username === "iac" && password === "password"){
         user = {id: 3, username:"iac", role: "iac", email: "iac@russelsmithgroup.com"};
       } */
-      //setTimeout(cb(user), 100) // fake async
-    },
-    signout(cb) {
-      setTimeout(cb, 100) // fake async
-    }
-}
+    //setTimeout(cb(user), 100) // fake async
+  },
+  signout(cb) {
+    setTimeout(cb, 100); // fake async
+  }
+};
 export default AclAuth;
--- src/middleware/stateLoader.js
+++ src/middleware/stateLoader.js
@@ -1,40 +1,36 @@
-"use strict"
+"use strict";
 
 class StateLoader {
-
-    loadState() {
-        try {
-            let serializedState = localStorage.getItem("http://localhost:8081:state");
-            if (serializedState === null) {
-                return this.initializeState();
-            }
-            return JSON.parse(serializedState);
-        }
-        catch (err) {
-            return this.initializeState();
-        }
+  loadState() {
+    try {
+      let serializedState = localStorage.getItem("http://localhost:8081:state");
+      if (serializedState === null) {
+        return this.initializeState();
+      }
+      return JSON.parse(serializedState);
+    } catch (err) {
+      return this.initializeState();
     }
+  }
 
-    saveState(state) {
-        try {
-            let serializedState = JSON.stringify(state);
-            localStorage.setItem("http://localhost:8081:state", serializedState);
-
-        }
-        catch (err) {
-        }
+  saveState(state) {
+    try {
+      let serializedState = JSON.stringify(state);
+      localStorage.setItem("http://localhost:8081:state", serializedState);
+    } catch (err) {
+      throw new Error(err);
     }
+  }
 
-    unsetState(){
-        localStorage.removeItem("http://localhost:8081:state");
-    }
+  unsetState() {
+    localStorage.removeItem("http://localhost:8081:state");
+  }
 
-    initializeState() {
-        return {
-              //state object
-            }
-        };
+  initializeState() {
+    return {
+      //state object
+    };
+  }
 }
-
 
 export default StateLoader;
