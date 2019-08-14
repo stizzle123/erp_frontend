833a30f42904879e8b283852bee5c6adec1eccae
Idowu <iarifayan@russelsmith.local>
2019-08-14 14:33:43 +0100

    changes made to middleware/api file

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -9,8 +9,8 @@ class MiddleWare {
 
   constructor(token = "") {
     this.api_root =
-      "http://ec2-18-223-2-36.us-east-2.compute.amazonaws.com:3000";
-    // this.api_root = "http://localhost:3000";
+      // "http://ec2-18-223-2-36.us-east-2.compute.amazonaws.com:3000";
+      this.api_root = "http://localhost:3000";
     this.token = token;
   }
 
