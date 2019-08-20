5acfa66f0596b681f72133762526ce10723248c5
Idowu <iarifayan@russelsmith.local>
2019-08-20 16:25:13 +0100

    Edit roles/permission logic

--- src/views/Roles/permission.js
+++ src/views/Roles/permission.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactTable from "react-table";
-import 'react-table/react-table.css'
+import "react-table/react-table.css";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
@@ -16,184 +16,200 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Check from "@material-ui/icons/Check";
 import Radio from "@material-ui/core/Radio";
 import Checkbox from "@material-ui/core/Checkbox";
-import { Redirect } from 'react-router'
+import { Redirect } from "react-router";
 import { cardTitle } from "assets/jss/material-dashboard-pro-react.jsx";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import purple from '@material-ui/core/colors/purple';
+import CircularProgress from "@material-ui/core/CircularProgress";
+import purple from "@material-ui/core/colors/purple";
 import { Link } from "react-router-dom";
-import {connect} from 'react-redux';
-import {AdminMenu} from "routes/dashboard.jsx";
+import { connect } from "react-redux";
+import { AdminMenu } from "routes/dashboard.jsx";
 import CardFooter from "../../components/Card/CardFooter.jsx";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import Button from "components/CustomButtons/Button.jsx";
-import * as roleActions from '../../actions/role.js';
+import * as roleActions from "../../actions/role.js";
 
 const styles = {
-    cardIconTitle: {
-      ...cardTitle,
-      marginTop: "15px",
-      marginBottom: "0px"
+  cardIconTitle: {
+    ...cardTitle,
+    marginTop: "15px",
+    marginBottom: "0px"
+  }
+};
+class Permission extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = { role: [{}], checked: [] };
+    this.handleSave.bind(this);
+  }
+
+  handleToggle(path) {
+    let checked = this.state.checked;
+    let index = checked.indexOf(path);
+    if (index == -1) {
+      checked.push(path);
+    } else {
+      checked.splice(index, 1);
     }
+    this.setState({ checked: checked });
+  }
+
+  handleSave = () => {
+    this.setState({ loading: true });
+    roleActions.savePermission(this.props, this.state, r => {
+      if (r) {
+        alert("Permission Saved");
+        this.setState({ loading: false });
+      }
+    });
   };
-  class Permission extends React.Component {
-  
-      constructor(props){
-          super(props)
-          this.props = props;
-          this.state = {role: [{}], checked: []}
-          this.handleSave.bind(this);
-      }
 
-      handleToggle(path){
-          let checked = this.state.checked;
-          let index = checked.indexOf(path);
-          if( index == -1){
-              checked.push(path)
-          }else{
-              checked.splice(index, 1);
-          }
-          this.setState({"checked": checked});
-       }
-       
-       handleSave = ()=> {
-           this.setState({loading:true});
-            roleActions.savePermission(this.props,this.state, (r)=>{
-                if(r){
-                    alert('Permission Saved');
-                    this.setState({loading:false});
-                }
-            });
-       }
+  componentDidMount() {
+    roleActions.findRoleById(this.props, this.props.match.params.id, r => {
+      this.setState({ role: r["0"], checked: r["0"].permission });
+    });
+  }
 
-      componentDidMount(){
-        roleActions.findRoleById(this.props, this.props.match.params.id,(r)=>{
-            this.setState({role:r['0'], checked:r['0'].permission})
-        });
+  parseMenu(routes, classes, allowedroutes, sub = false) {
+    let allowed = typeof allowedroutes == "undefined" ? [] : allowedroutes;
+    let w = sub ? 12 : 4;
+    return routes.map((prop, key) => {
+      return (
+        <GridItem xs={w} sm={w} md={w} key={key}>
+          <div className={classes.checkboxAndRadio}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={allowed.indexOf(prop.path) >= 0 ? true : false}
+                  tabIndex={-1}
+                  onClick={() => this.handleToggle(prop.path)}
+                  checkedIcon={<Check className={classes.checkedIcon} />}
+                  icon={<Check className={classes.uncheckedIcon} />}
+                  classes={{
+                    checked: classes.checked
+                  }}
+                />
+              }
+              classes={{
+                label: classes.label
+              }}
+              label={prop.name}
+            />
+            {prop.collapse && prop.views.length > 0
+              ? this.parseMenu(prop.views, classes, allowed, true)
+              : ""}
+            {typeof prop.actions != "undefined" && prop.actions.length > 0
+              ? this.parseActions(prop.actions, prop.path, classes, allowed)
+              : ""}
+          </div>
+        </GridItem>
+      );
+    });
+  }
 
-      }
+  parseActions(actions, path, classes, allowed) {
+    return actions.map((prop, key) => {
+      return (
+        <GridItem xs={12} sm={12} md={12} key={key}>
+          <div className={classes.checkboxAndRadio}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={
+                    allowed.indexOf(path + "/" + prop) >= 0 ? true : false
+                  }
+                  tabIndex={-1}
+                  onClick={() => this.handleToggle(path + "/" + prop)}
+                  checkedIcon={<Check className={classes.checkedIcon} />}
+                  icon={<Check className={classes.uncheckedIcon} />}
+                  classes={{
+                    checked: classes.checked
+                  }}
+                />
+              }
+              classes={{
+                label: classes.label
+              }}
+              label={prop}
+            />
+          </div>
+        </GridItem>
+      );
+    });
+  }
 
-      parseMenu(routes, classes, allowedroutes, sub=false ){
-        let allowed = (typeof allowedroutes == "undefined")? [] : allowedroutes;
-        let w = (sub)? 12 : 4;
-       return routes.map((prop, key) => {
-               return( <GridItem xs={w} sm={w} md={w} >
-                    <div className={classes.checkboxAndRadio}>
-                        <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={(allowed.indexOf(prop.path) >= 0)? true : false}
-                                        tabIndex={-1}
-                                        onClick={() => this.handleToggle(prop.path)}
-                                        checkedIcon={
-                                            <Check className={classes.checkedIcon} />
-                                        }
-                                        icon={<Check className={classes.uncheckedIcon} />}
-                                        classes={{
-                                        checked: classes.checked
-                                        }}
-                                    />
-                                }
-                                classes={{
-                                    label: classes.label
-                                }}
-                                label={(prop.name)}
-                        />
-                        {(prop.collapse && prop.views.length > 0)? this.parseMenu(prop.views, classes, allowed, true) : ''}
-                        {((typeof prop.actions != "undefined") && prop.actions.length > 0)? this.parseActions(prop.actions, prop.path ,classes, allowed) : ''}
-                    </div>
-                </GridItem>)
-        });
-      }
-
-      parseActions(actions, path, classes, allowed){
-        return actions.map((prop, key) => {
-            return( <GridItem xs={12} sm={12} md={12} >
-                 <div className={classes.checkboxAndRadio}>
-                     <FormControlLabel
-                             control={
-                                 <Checkbox
-                                     checked={(allowed.indexOf(path+"/"+prop) >= 0)? true : false}
-                                     tabIndex={-1}
-                                     onClick={() => this.handleToggle(path+"/"+prop)}
-                                     checkedIcon={
-                                         <Check className={classes.checkedIcon} />
-                                     }
-                                     icon={<Check className={classes.uncheckedIcon} />}
-                                     classes={{
-                                     checked: classes.checked
-                                     }}
-                                 />
-                             }
-                             classes={{
-                                 label: classes.label
-                             }}
-                             label={(prop)}
-                     />
-                 </div>
-             </GridItem>)
-        })
-      }
-
-      render(){
-        const { classes } = this.props;
-        if(this.state.loading){
-            return (
-              <div>
+  render() {
+    const { classes } = this.props;
+    if (this.state.loading) {
+      return (
+        <div>
+          <Grid container>
+            <GridItem xs={12} sm={6} md={3} style={{ margin: "20% auto" }}>
+              <CircularProgress
+                className={classes.progress}
+                size={70}
+                style={{ color: purple[500] }}
+                thickness={10}
+              />
+            </GridItem>
+          </Grid>
+        </div>
+      );
+    } else {
+      return (
+        <GridContainer>
+          <GridItem xs={12}>
+            <Card>
+              <CardHeader color="primary" icon>
+                <CardIcon color="primary">
+                  <Assignment />
+                </CardIcon>
+                <h4 className={classes.cardIconTitle}>
+                  Permission for Role : {this.state.role.name}
+                </h4>
+              </CardHeader>
+              <CardBody>
+                <form
+                  className={classes.container}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <GridContainer
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                  >
+                    {this.parseMenu(AdminMenu, classes, this.state.checked)}
+                  </GridContainer>
+                </form>
+              </CardBody>
+              <CardFooter>
                 <Grid container>
-                  <GridItem xs={12} sm={6} md={3} style={{ margin:'20% auto'}}>
-                    <CircularProgress className={classes.progress} size={70} style={{ color: purple[500]}} thickness={10} />
+                  <GridItem xs={12} sm={6} md={2}>
+                    <Button color="primary" onClick={this.handleSave}>
+                      Save
+                    </Button>
                   </GridItem>
                 </Grid>
-              </div>)
-          }else{
-            return (<GridContainer>
-                <GridItem xs={12}>
-                    <Card>
-                        <CardHeader color="primary" icon>
-                        <CardIcon color="primary">
-                            <Assignment />
-                        </CardIcon>
-                        <h4 className={classes.cardIconTitle}>
-                        Permission for Role : {this.state.role.name}
-                        </h4>
-                        </CardHeader>
-                        <CardBody>
-                            <form className={classes.container} noValidate autoComplete="off">
-                                <GridContainer direction="row" justify="center" alignItems="center">
-                                    { 
-                                        this.parseMenu(AdminMenu, classes, this.state.checked)
-                                    }   
-                                </GridContainer>                                                                                        
-                            </form>
-                        </CardBody>
-                        <CardFooter>
-                            <Grid container>
-                                <GridItem xs={12} sm={6} md={2}>
-                                <Button color="primary" onClick={this.handleSave}>Save</Button>
-                                </GridItem>
-                            </Grid>
-                        </CardFooter>
-                    </Card>
-                </GridItem>
-            </GridContainer>);
-          }
-      }
-
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      );
     }
+  }
+}
 
-    Permission.defaultProps = {
-       
-    }
+Permission.defaultProps = {};
 
-    function mapStateToProps(state) {
-        return {
-          loader: state.loader,
-          user: state.auth.user,
-        };
-    }
-      
-      
-    export default connect(
-        mapStateToProps,
-        null
-    )(withStyles(styles)(Permission));
+function mapStateToProps(state) {
+  return {
+    loader: state.loader,
+    user: state.auth.user
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(withStyles(styles)(Permission));
--- src/views/Vendor/view/workReferences.js
+++ src/views/Vendor/view/workReferences.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import GridItem from "../../../components/Grid/GridItem.jsx";
 import CustomInput from "../../../components/CustomInput/CustomInput.jsx";
 import Button from "../../../components/CustomButtons/Button.jsx";
@@ -12,7 +12,7 @@ import CardBody from "../../../components/Card/CardBody.jsx";
 import CardFooter from "../../../components/Card/CardFooter.jsx";
 import Progress from "../../../components/Progress/Progress.jsx";
 import MiddleWare from "../../../middleware/api";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 const styles = {
@@ -38,145 +38,174 @@ class WorkReferences extends React.Component {
     redirect: false
   };
 
-
-
   render() {
     const { classes } = this.props;
-    return  (this.props.redirect)? 
-    <Redirect to="/dashboard" /> 
-    : 
-    (
+    return this.props.redirect ? (
+      <Redirect to="/dashboard" />
+    ) : (
       <Grid container>
-      <GridItem xs={12} sm={12} md={12}>
-      <form className={classes.container} noValidate autoComplete="off">
-        <Progress loading={this.props.loading}/>
-        <Card>
-            <CardBody>
-            <Grid container>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Company Name" id="coy_name" required
+        <GridItem xs={12} sm={12} md={12}>
+          <form className={classes.container} noValidate autoComplete="off">
+            <Progress loading={this.props.loading} />
+            <Card>
+              <CardBody>
+                <Grid container>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Company Name"
+                      id="coy_name"
+                      required
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.coy_name
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Company Address" id="coy_address" required
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Company Address"
+                      id="coy_address"
+                      required
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.coy_address
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Contact Person" id="contact_person" required
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Contact Person"
+                      id="contact_person"
+                      required
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.contact_person
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Contact Designation"
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Contact Designation"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.contact_designation
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Contact Email"
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Contact Email"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.contact_email
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Contact Phone"
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Contact Phone"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.contact_phone
                       }}
                     />
-                </GridItem>
-            </Grid>
-            </CardBody>
-            <h4 style={{ marginLeft:"20px"}}>Individual Reference.</h4>
-            <CardBody>
-            <Grid container>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Name" 
+                  </GridItem>
+                </Grid>
+              </CardBody>
+              <h4 style={{ marginLeft: "20px" }}>Individual Reference.</h4>
+              <CardBody>
+                <Grid container>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Name"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.name
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Address"
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Address"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.address
                       }}
                     />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Email"
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Email"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.email
                       }}
                     />
-                </GridItem>    
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput labelText="Phone" 
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Phone"
                       formControlProps={{
                         fullWidth: true
-                      }} inputProps={{
+                      }}
+                      inputProps={{
                         disabled: true,
                         value: this.props.data.phone
                       }}
                     />
-                </GridItem>
-              </Grid>
+                  </GridItem>
+                </Grid>
               </CardBody>
             </Card>
-            </form>
-          </GridItem>
-        </Grid>
-    )
+          </form>
+        </GridItem>
+      </Grid>
+    );
   }
 }
 
 WorkReferences.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    data: (typeof(state.vendor.work_reference) != 'undefined')?state.vendor.work_reference: {},
+    data:
+      typeof state.vendor.work_reference != "undefined"
+        ? state.vendor.work_reference
+        : {},
     user: state.auth.user
   };
 }
 
-export default connect(mapStateToProps, null)(withStyles(styles)(WorkReferences));
+export default connect(
+  mapStateToProps,
+  null
+)(withStyles(styles)(WorkReferences));
